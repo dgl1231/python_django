c2d8fd03d18553caa2f763cf53fbc43a47104984
DongGeon Lee <63222048+dgl1231@users.noreply.github.com>
2023-02-06 17:58:36 +0900

    feat : python

--- python_polls/polls/models.py
+++ python_polls/polls/models.py
@@ -23,3 +23,18 @@ class Choice(models.Model):
 
     def __str__(self):
         return self.choice_text
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)    #post's title
+    content = models.TextField()                #post's contents
+
+
+class Article(models.Model):
+    title = models.CharField(max_length=100)        #Article's title
+    content = models.TextField()                    #Article's contents
+    created_at = models.DateTimeField(auto_now_add=True)    #create datetime
+    updated_at = models.DateTimeField(auto_now=True)        #update datetime
+
+
+
